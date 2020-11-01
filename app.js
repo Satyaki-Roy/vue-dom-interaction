85d912455aa92c248657f4ff5cd367c610234df6
Satyaki Roy <satyakiroy001@gmail.com>
2020-11-01 13:01:08 +0530

    dynamic styling

--- app.js
+++ app.js
@@ -1,54 +1,22 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: "",
-      lastName: "",
+      selectedBoxA: false,
+      selectedBoxB: false,
+      selectedBoxC: false,
     };
   },
-  watch: {
-    counter(newValue, oldValue) {
-      if (newValue > 50) {
-        this.counter = 0;
-      }
-    },
-    /*
-    name(newValue, oldValue) {
-      if (this.name === "") {
-        this.fullName = "";
-      } else {
-        this.fullName = `${newValue} ${this.lastName}`;
-      }
-    },
-    lastName(newValue, oldValue) {
-      if (this.name === "") {
-        this.fullName = "";
-      } else {
-        this.fullName = `${this.name} ${newValue}`;
-      }
-    },
-    */
-  },
-  computed: {
-    fullName() {
-      return `${this.name} ${this.lastName}`;
-    },
-  },
   methods: {
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    remove(num) {
-      this.counter = this.counter - num;
-    },
-    resetInput() {
-      this.name = "";
-      this.lastName = "";
-    },
-    submitForm() {
-      alert("Submitted");
+    boxSelected(box) {
+      if (box === "A") {
+        this.selectedBoxA = true;
+      } else if (box === "B") {
+        this.selectedBoxB = true;
+      } else if (box === "C") {
+        this.selectedBoxC = true;
+      }
     },
   },
 });
 
-app.mount("#events");
+app.mount("#styling");
